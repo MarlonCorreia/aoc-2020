eb01496cf1683db42a2a2916a6d8606a5b913a29
marloncorreia <malilocsimas26@gmail.com>
2020-12-06 15:32:24 -0300

    change if order

--- day-4/day4.py
+++ day-4/day4.py
@@ -12,13 +12,13 @@ def part2(file):
     qtd = 0 
     for key in file:
         if set(key).issuperset(set(obligatory)):
-            if len(key["pid"]) == 9:
+            if ("cm" in key["hgt"] and 193 >= int(key["hgt"].strip("cm")) >= 150) or ("in" in key["hgt"] and 76 >= int(key["hgt"].strip("in")) >= 59):
                 if key["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
-                    if re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', key["hcl"]):
-                        if ("cm" in key["hgt"] and 193 >= int(key["hgt"].strip("cm")) >= 150) or ("in" in key["hgt"] and 76 >= int(key["hgt"].strip("in")) >= 59):
-                            if 2030 >= int(key["eyr"]) >= 2020:
-                                if 2020 >= int(key["iyr"]) >= 2010:
-                                    if 2002 >= int(key["byr"]) >= 1920:
+                    if re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', key["hcl"]):                               
+                        if 2030 >= int(key["eyr"]) >= 2020:
+                            if 2020 >= int(key["iyr"]) >= 2010:
+                                if 2002 >= int(key["byr"]) >= 1920:
+                                    if len(key["pid"]) == 9:
                                         qtd += 1                            
     return qtd
 
